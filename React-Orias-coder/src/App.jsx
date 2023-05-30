836457f9191df40582293ed2a1f8f1b8431392df
Maximiliano Orias <maxiorias@gmail.com>
2023-05-29 21:37:16 -0300

    logo

--- React-Orias-coder/src/App.jsx
+++ React-Orias-coder/src/App.jsx
@@ -1,12 +1,18 @@
+import React from 'react';
+import { AppBar, Toolbar, Typography } from '@mui/material';
 import Header from "./components/Header"
 import './App.css'
+
 
 function App() {
   
   return (
+    
     <div>
-      <Header />
+      <Header  />
     </div>
+   
+  
   )
 }
 
--- React-Orias-coder/src/components/Header.jsx
+++ React-Orias-coder/src/components/Header.jsx
@@ -1,18 +1,32 @@
+import React from 'react';
+import { AppBar, Toolbar, Typography } from '@mui/material';
+
 const Header = () => {
-
     return (
-        <header className="header">
-            <div className="header__container">
-                <h1 className="header__logo"> LOGO </h1>
+     
 
-                <nav className="header__nav">
-                    <a className="header__link" href="#">Enlace 1</a> 
-                    <a className="header__link" href="#">Enlace 2</a> 
-                    <a className="header__link" href="#">Enlace 3</a>
-                </nav>
-            </div>
-        </header>
-    )
-}
-
-export default Header
+      <AppBar position="static" className='header' sx={{ backgroundColor: '#333333' }} >
+        <Toolbar>
+        <img src="../public/vite.svg"alt="Logo" className="header__logo" />
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1  }}>
+            Kitsune
+          </Typography>
+  
+          <nav className="header__nav">
+            <a className="header__link" href="#">
+              Inicio
+            </a>
+            <a className="header__link" href="#">
+              Productos
+            </a>
+            <a className="header__link" href="#">
+              Contacto
+            </a>
+          </nav>
+        </Toolbar>
+      </AppBar>
+    
+    );
+  };
+  
+  export default Header;
