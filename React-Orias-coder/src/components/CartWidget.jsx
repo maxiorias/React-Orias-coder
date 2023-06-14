a097bf94295de9bae1a3504703f158dfca0db90b
Maximiliano Orias <maxiorias@gmail.com>
2023-06-14 20:44:54 -0300

    useState y Router

--- React-Orias-coder/src/components/CartWidget.jsx
+++ React-Orias-coder/src/components/CartWidget.jsx
@@ -2,12 +2,12 @@ import React, { useState } from 'react';
 import { ShoppingCart } from '@mui/icons-material';
 
 const CartWidget = () => {
-  const [cartItems, setCartItems] = useState(0); // Estado para almacenar el número de elementos en el carrito
+  const [cartItems, setCartItems] = useState(0); 
 
   return (
     <div className="cart-widget">
       <ShoppingCart className="cart-widget__icon" />
-      <span className="cart-widget__notification">{cartItems}</span> {/* Mostrar el número de elementos en el carrito */}
+      <span className="cart-widget__notification">{cartItems}</span> 
     </div>
   );
 };
--- React-Orias-coder/src/components/formulario.jsx
+++ React-Orias-coder/src/components/formulario.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { TextField, Button } from '@mui/material';
 import './Formulario.css';
 
